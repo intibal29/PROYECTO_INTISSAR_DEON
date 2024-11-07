312cce20fb3c46ddab334b9b226ad987aa699f9f
intibal29 <117351402+intibal29@users.noreply.github.com>
2024-11-07 22:59:52 +0100

    conexion bbdd

---
+++ src/main/java/com/intissar/olimpiadas/db/DBConnect.java
@@ -0,0 +1,59 @@
+package com.intissar.olimpiadas.db;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnect {
+    private final Connection connection;
+
+    public DBConnect() throws SQLException {
+        Properties configuracion = getConfiguration();
+        Properties connConfig = new Properties();
+        connConfig.setProperty("user", configuracion.getProperty("user"));
+        connConfig.setProperty("password", configuracion.getProperty("password"));
+
+        String url = "jdbc:mysql://" + configuracion.getProperty("address") + ":" +
+                configuracion.getProperty("port") + "/" +
+                configuracion.getProperty("database") +
+                "?serverTimezone=Europe/Madrid";
+
+        connection = DriverManager.getConnection(url, connConfig);
+        connection.setAutoCommit(true);
+
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+        System.out.println("--- Datos de conexión ------------------------------------------");
+        System.out.printf("Base de datos: %s%n", databaseMetaData.getDatabaseProductName());
+        System.out.printf("  Versión: %s%n", databaseMetaData.getDatabaseProductVersion());
+        System.out.printf("Driver: %s%n", databaseMetaData.getDriverName());
+        System.out.printf("  Versión: %s%n", databaseMetaData.getDriverVersion());
+        System.out.println("----------------------------------------------------------------");
+    }
+
+    public static Properties getConfiguration() {
+        File f = new File("configuration.properties");
+        Properties properties = new Properties();
+        try (FileInputStream configFileReader = new FileInputStream(f)) {
+            properties.load(configFileReader);
+        } catch (IOException e) {
+            System.out.println("Error al cargar la configuración: " + e.getMessage());
+            throw new RuntimeException("configuration.properties no encontrado en la ruta: " + f.getPath());
+        }
+        return properties;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Connection closeConnection() throws SQLException {
+        connection.close();
+        return connection;
+    }
+
+}
